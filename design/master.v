2705a92130ea2aa9bfbad2360438a7ce33977473
nitesh15ks-a11y <nitesh15ks@gmail.com>
2025-10-27 21:49:41 +0530

    Update master.v

--- design/master.v
+++ design/master.v
@@ -6,8 +6,7 @@ module spi_master(
     input  wire [7:0] data_in,// 8-bit data input 
     input  wire miso,         // Master In Slave out
     output reg  mosi,         // Master Out Slave in 
-    output reg  cs0,cs1,cs2,
+    output reg  cs0,cs1,cs2,  // slave select
     output reg  sclk,         // SPI clock
-    output reg  ss,           // Slave Select (active low)
     output reg  [7:0] data_out
 );
