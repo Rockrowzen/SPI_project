51c5f8b06066ee99286b6f97c0c757143c491487
Rockrowzen <rengashyam.r@google.com>
2025-10-28 14:57:38 +0530

    updated slaves

--- design/slave.v
+++ design/slave.v
@@ -1,4 +1,18 @@
-module spi_slave (
+module spi_slave_1(
+    input  wire sclk,
+    input  wire ss,          // Active low
+    input  wire mosi,
+    output reg  miso,
+    output reg [7:0] data_out
+);
+module spi_slave_2(
+    input  wire sclk,
+    input  wire ss,          // Active low
+    input  wire mosi,
+    output reg  miso,
+    output reg [7:0] data_out
+);
+module spi_slave_3(
     input  wire sclk,
     input  wire ss,          // Active low
     input  wire mosi,
